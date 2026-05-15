a72e5bbfd499f1130f9bd38a4062c34d8be2e028
vishal thakur <vishal.wartiz14@gmail.com>
2026-05-15 17:15:41 +0530

    fix tailwind config content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+
+        // ✅ IMPORTANT: added JS paths for Vite
+        './resources/js/**/*.js',
+        './resources/**/*.js',
     ],
 
     theme: {
