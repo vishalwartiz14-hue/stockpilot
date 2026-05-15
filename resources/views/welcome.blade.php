da55f63cee1fceef829607196475b005a9fe3b74
vishal thakur <vishal.wartiz14@gmail.com>
2026-05-15 16:58:16 +0530

    fix vite placement for railway css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-@vite(['resources/css/app.css', 'resources/js/app.js'])
 <head>
+   
+
     <meta charset="UTF-8">
     <title>AI Restaurant Inventory System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"> -->
     <style>
         body {
