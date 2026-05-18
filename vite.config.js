ea50a480fafcb74b4e2cc3a253ec93601efded7c
vishal thakur <vishal.wartiz14@gmail.com>
2026-05-18 13:21:52 +0530

    fix mixed content issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-     base: '/',
+    base: '/',
     plugins: [
         laravel({
             input: [
