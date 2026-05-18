be433f5799b932eeda2f460d6d4eed855ac0b498
vishal thakur <vishal.wartiz14@gmail.com>
2026-05-18 14:56:00 +0530

    fix mixed content http https assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,11 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        // Force HTTPS in production (fix mixed content issue)
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+   public function boot(): void
+{
+    if (app()->environment('production')) {
+        request()->server->set('HTTPS', 'on');
+        \URL::forceScheme('https');
     }
+}
 }
